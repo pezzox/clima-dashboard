1657fae6fe2efa37f9e8b94ff04a443fc3e32ca0
Federico <pezzotti.federico00@gmail.com>
2025-07-01 21:44:07 +0200

    Updates

--- assets/Calendar-C2cDeL7n.js
+++ assets/Calendar-C2cDeL7n.js
@@ -1,4 +1,4 @@
-import{r as U,R as ws,j as ut}from"./index-DIpcyU2g.js";/*!
+import{r as U,R as ws,j as ut}from"./index-BWPkr4Ga.js";/*!
  * @kurkle/color v0.3.4
  * https://github.com/kurkle/color#readme
  * (c) 2024 Jukka Kurkela
