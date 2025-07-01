6efd3ce164200c8baf1e45a2ffd179953409eb59
Federico <pezzotti.federico00@gmail.com>
2025-07-01 21:40:18 +0200

    Updates

--- assets/Calendar-Cy3XDdX3.js
+++ assets/Calendar-Cy3XDdX3.js
@@ -1,4 +1,4 @@
-import{r as U,R as ws,j as ut}from"./index-CVx00l08.js";/*!
+import{r as U,R as ws,j as ut}from"./index-Bw9fDM0M.js";/*!
  * @kurkle/color v0.3.4
  * https://github.com/kurkle/color#readme
  * (c) 2024 Jukka Kurkela
