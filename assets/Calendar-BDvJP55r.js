494813f25966e1d29ebb4dde2ba1791201a33ca7
Federico <pezzotti.federico00@gmail.com>
2025-07-01 17:42:42 +0200

    Updates

--- assets/Calendar-BDvJP55r.js
+++ assets/Calendar-BDvJP55r.js
@@ -1,4 +1,4 @@
-import{r as U,R as ws,j as ut}from"./index-veLUViV8.js";/*!
+import{r as U,R as ws,j as ut}from"./index-BqRFZAkt.js";/*!
  * @kurkle/color v0.3.4
  * https://github.com/kurkle/color#readme
  * (c) 2024 Jukka Kurkela
