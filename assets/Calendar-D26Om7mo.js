dc4571516972ffdb4d7e2356a1e237ae54d9ce42
Federico <pezzotti.federico00@gmail.com>
2025-07-01 16:22:23 +0200

    Updates

--- assets/Calendar-D26Om7mo.js
+++ assets/Calendar-D26Om7mo.js
@@ -1,4 +1,4 @@
-import{r as U,R as ws,j as ut}from"./index-DLY4yuAS.js";/*!
+import{r as U,R as ws,j as ut}from"./index-CKlbVjuI.js";/*!
  * @kurkle/color v0.3.4
  * https://github.com/kurkle/color#readme
  * (c) 2024 Jukka Kurkela
--- assets/lodash-CQTZgbPV.js
+++ assets/lodash-CQTZgbPV.js
@@ -1,4 +1,4 @@
-import{c as Jr,g as ip}from"./index-DLY4yuAS.js";var jt={exports:{}};/**
+import{c as Jr,g as ip}from"./index-CKlbVjuI.js";var jt={exports:{}};/**
  * @license
  * Lodash <https://lodash.com/>
  * Copyright OpenJS Foundation and other contributors <https://openjsf.org/>
