b0aa6e6a6a35cb5736cfed3759b4340e4797d00e
Federico <pezzotti.federico00@gmail.com>
2025-07-01 21:50:46 +0200

    Updates

--- assets/Calendar-CKQlA_TJ.js
+++ assets/Calendar-CKQlA_TJ.js
@@ -1,4 +1,4 @@
-import{r as U,R as ws,j as ut}from"./index-wETUQtCZ.js";/*!
+import{r as U,R as ws,j as ut}from"./index-CHMXcF0D.js";/*!
  * @kurkle/color v0.3.4
  * https://github.com/kurkle/color#readme
  * (c) 2024 Jukka Kurkela
