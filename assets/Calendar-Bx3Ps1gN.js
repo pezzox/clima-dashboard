0738bf44dad51bdf2e88b6db63fc259bb8760b07
Federico <pezzotti.federico00@gmail.com>
2025-07-01 21:18:14 +0200

    Updates

--- assets/Calendar-Bx3Ps1gN.js
+++ assets/Calendar-Bx3Ps1gN.js
@@ -1,4 +1,4 @@
-import{r as U,R as ws,j as ut}from"./index-By7WrmxZ.js";/*!
+import{r as U,R as ws,j as ut}from"./index-D2sk_tLi.js";/*!
  * @kurkle/color v0.3.4
  * https://github.com/kurkle/color#readme
  * (c) 2024 Jukka Kurkela
--- assets/lodash-BR-wUXfC.js
+++ assets/lodash-BR-wUXfC.js
@@ -1,4 +1,4 @@
-import{c as Jr,g as ip}from"./index-By7WrmxZ.js";var jt={exports:{}};/**
+import{c as Jr,g as ip}from"./index-D2sk_tLi.js";var jt={exports:{}};/**
  * @license
  * Lodash <https://lodash.com/>
  * Copyright OpenJS Foundation and other contributors <https://openjsf.org/>
