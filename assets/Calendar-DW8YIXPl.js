987e88762570ede9cdaadb18a44c1a18b48132a6
Federico <pezzotti.federico00@gmail.com>
2025-07-01 21:15:16 +0200

    Updates

--- assets/Calendar-DW8YIXPl.js
+++ assets/Calendar-DW8YIXPl.js
@@ -1,4 +1,4 @@
-import{r as U,R as ws,j as ut}from"./index-byyUcLj8.js";/*!
+import{r as U,R as ws,j as ut}from"./index-By7WrmxZ.js";/*!
  * @kurkle/color v0.3.4
  * https://github.com/kurkle/color#readme
  * (c) 2024 Jukka Kurkela
--- assets/lodash-DrKLtQmE.js
+++ assets/lodash-DrKLtQmE.js
@@ -1,4 +1,4 @@
-import{c as Jr,g as ip}from"./index-byyUcLj8.js";var jt={exports:{}};/**
+import{c as Jr,g as ip}from"./index-By7WrmxZ.js";var jt={exports:{}};/**
  * @license
  * Lodash <https://lodash.com/>
  * Copyright OpenJS Foundation and other contributors <https://openjsf.org/>
