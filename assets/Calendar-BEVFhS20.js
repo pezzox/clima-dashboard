43feb00b5bb67fa506563f9f508a7788a8c59f51
Federico <pezzotti.federico00@gmail.com>
2025-07-01 21:38:13 +0200

    Updates

--- assets/Calendar-BEVFhS20.js
+++ assets/Calendar-BEVFhS20.js
@@ -1,4 +1,4 @@
-import{r as U,R as ws,j as ut}from"./index-MhvwTrgk.js";/*!
+import{r as U,R as ws,j as ut}from"./index-CVx00l08.js";/*!
  * @kurkle/color v0.3.4
  * https://github.com/kurkle/color#readme
  * (c) 2024 Jukka Kurkela
