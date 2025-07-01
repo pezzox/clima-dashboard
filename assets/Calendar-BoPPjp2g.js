37fc1e6d5a55a75386bb4820866698f10857c87b
Federico <pezzotti.federico00@gmail.com>
2025-07-01 20:51:47 +0200

    Updates

--- assets/Calendar-BoPPjp2g.js
+++ assets/Calendar-BoPPjp2g.js
@@ -1,4 +1,4 @@
-import{r as U,R as ws,j as ut}from"./index-ClnaM-GC.js";/*!
+import{r as U,R as ws,j as ut}from"./index-BLco6VX2.js";/*!
  * @kurkle/color v0.3.4
  * https://github.com/kurkle/color#readme
  * (c) 2024 Jukka Kurkela
