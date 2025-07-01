707de0111d9cc0cc1ad85194ce75dd122a662122
Federico <pezzotti.federico00@gmail.com>
2025-07-01 21:20:21 +0200

    Updates

--- assets/Calendar-olWLpGZV.js
+++ assets/Calendar-olWLpGZV.js
@@ -1,4 +1,4 @@
-import{r as U,R as ws,j as ut}from"./index-D2sk_tLi.js";/*!
+import{r as U,R as ws,j as ut}from"./index-0Lq8Qvzj.js";/*!
  * @kurkle/color v0.3.4
  * https://github.com/kurkle/color#readme
  * (c) 2024 Jukka Kurkela
